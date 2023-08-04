814f807069dee2c18cdc72a8ad86381749efab7b
DianaAvilesCh <diana.aviles2017@uteq.edu.ec>
2023-08-04 00:08:31 -0500

    Login

--- resources/views/Log_in.blade.php
+++ resources/views/Log_in.blade.php
@@ -1,13 +1,14 @@
 @include('layouts.Assets')
 <main>
     <form method="POST">
-        <div class="ct" >
+        <div class="ct">
             <div class="card c-round" style="max-width: 60%; ">
-                <div class="row g-0 card-pr">
+                <div class="row g-0">
                     <div class="col-md-4 ct-imagen">
                         <img src="https://cdn-icons-png.flaticon.com/512/4697/4697260.png" class="img-fluid rounded-start">
                     </div>
-                    <div class="col-sm-8 col-sm-6 body-c">
+                    <div class="col-12 col-sm-8 body-c">
+                        <br />
                         <div class="card-body">
                             <h2 class="card-title text-c">Iniciar Sesión</h2>
                             <div class="mb-3">
@@ -18,12 +19,28 @@
                                 <label for="inputPassword" class="form-label text-c">Contraseña</label>
                                 <input type="password" class="form-control" id="inputPassword" placeholder="Ingrese su contraseña">
                             </div>
-                            <div class="mb-3 form-check">
-                                <input type="checkbox" class="form-check-input" id="check1">
-                                <label class="form-check-label text-c" for="check1">Recordarme</label>
+                            <div class="container">
+                            <div class="row">
+                                <div class="form-check col-md-auto">
+                                    <input type="checkbox" class="form-check-input" id="check1">
+                                    <label class="form-check-label text-c" for="check1">Recordarme</label>
+                                </div>
+                                <div class="col" style="text-align: end;">
+                                        <a href="#" class="text-c" for="forgot">¿Se te olvido la contraseña?</a>
+                                    </div>
                             </div>
-                            <button type="submit" class="btn btn-p">Submit</button>
+                            </div>
+                            <div class="mb-3" style="text-align: center;">
+                            <br />
+                            <button type="submit" class="btn btn-p" style="text-align: center;">Submit</button>
+                            </div>
+                            
+                            <div class="mb-3" style="text-align: center;">
+                                <label class="text-c" for="forgot">¿No tienes cuenta?</label>
+                                <a href="#" class="text-c" for="forgot">Registrate</a>
+                            </div>
                         </div>
+                        <br />
                     </div>
                 </div>
             </div>
