9dd35cda131be58ef8b3a5e74639b312951fa8a1
DianaAvilesCh <69444307+DianaAvilesCh@users.noreply.github.com>
2023-09-03 17:44:01 -0500

    Update app.php

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://tutor-now-4fbb17a4759c.herokuapp.com'),
 
     'asset_url' => env('ASSET_URL'),
 
